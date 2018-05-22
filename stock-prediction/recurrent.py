d5babf7c044f6ae29b0552833807592e9f22a7f5
theawless <theawless@gmail.com>
2018-05-23 00:26:34 +0530

    Add stacked lstm

--- stock-prediction/recurrent.py
+++ stock-prediction/recurrent.py
@@ -14,6 +14,55 @@ import tqdm
 import feature
 
 
+def recurrent_name(recurrent):
+    return ' '.join(recurrent.__name__.split('_'))
+
+
+def recurrent_stacked_lstm(X_train, X_test, y_train):
+    """
+    Recurrent stacked LSTM network.
+    :param X_train: train features
+    :param X_test: test features
+    :param y_train: train output
+    :return: predicted output
+    """
+    network = keras.Sequential()
+    network.add(keras.layers.LSTM(64, input_shape=X_train.shape[1:], return_sequences=True))
+    network.add(keras.layers.LSTM(64, return_sequences=True, dropout=0.2, recurrent_dropout=0.2))
+    network.add(keras.layers.LSTM(64, dropout=0.2, recurrent_dropout=0.2))
+    network.add(keras.layers.core.Dense(1))
+    network.compile(loss="mse", optimizer="rmsprop")
+
+    network.fit(X_train, y_train, batch_size=512, epochs=25, validation_split=0.05)
+    y_pred = network.predict(X_test)
+    return y_pred
+
+
+def recurrent_lstm(X_train, X_test, y_train):
+    """
+    Recurrent LSTM network.
+    :param X_train: train features
+    :param X_test: test features
+    :param y_train: train output
+    :return: predicted output
+    """
+    network = keras.Sequential()
+    network.add(keras.layers.LSTM(64, input_shape=X_train.shape[1:], dropout=0.2, recurrent_dropout=0.2))
+    network.add(keras.layers.core.Dense(1))
+    network.compile(loss="mse", optimizer="rmsprop")
+
+    network.fit(X_train, y_train, batch_size=512, epochs=25, validation_split=0.05)
+    y_pred = network.predict(X_test)
+    return y_pred
+
+
+def get_recurrent_predictors():
+    return [
+        recurrent_lstm,
+        recurrent_stacked_lstm,
+    ]
+
+
 def build_sequence(X, y, size):
     Xs = []
     for i in range(X.shape[0] - size - 1):
@@ -21,23 +70,37 @@ def build_sequence(X, y, size):
     return numpy.array(Xs), numpy.array(y[size + 1:])
 
 
-def recurrent_lstm(X_train, X_test, y_train, y_test):
+def plot_recurrent_for_multiple(data, n_test, n_past):
     """
-    Recurrent LSTM network.
-    :param X_train: train features
-    :param X_test: test features
-    :param y_train: train output
-    :param y_test: test output
-    :return: predicted output
+    Plot the predictions for various recurrent techniques for multiple days in future.
+    :param data: quandl data
+    :param n_test: number of days for testing
+    :param n_past: number of days to remember in context
     """
-    network = keras.Sequential()
-    network.add(keras.layers.LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(X_train.shape[1:])))
-    network.add(keras.layers.core.Dense(1))
-    network.compile(loss="mse", optimizer="rmsprop")
+    X, y = feature.build_sets(data, n_test)
+    X_train, X_test, y_train, y_test = feature.divide_sets(X, y, n_test + n_past + 1)
+    X_test, y_test = build_sequence(X_test, y_test, n_past)
+    for recurrent in get_recurrent_predictors():
+        y_pred_combined = []
+        for day in tqdm.tqdm(range(1, n_test + 1)):
+            X, y = feature.build_sets(data, day)
+            X_train, X_test, y_train, y_test = feature.divide_sets(X, y, n_test + n_past + 1)
+            X_train, y_train = build_sequence(X_train, y_train, n_past)
+            X_test, y_test = build_sequence(X_test, y_test, n_past)
 
-    network.fit(X_train, y_train, batch_size=512, epochs=1, validation_split=0.05)
-    y_pred = network.predict(X_test)
-    return y_pred
+            y_pred = recurrent(X_train, X_test, y_train)
+            y_pred_combined.append(y_pred[day - 1])
+        r2_score = sklearn.metrics.r2_score(y_test, y_pred_combined)
+
+        print(recurrent_name(recurrent), "r2_score", r2_score)
+        matplotlib.pyplot.plot(y_pred_combined, label=recurrent_name(recurrent))
+    matplotlib.pyplot.plot(y_test, label="actual")
+    matplotlib.pyplot.title("Predictions of recurrent for days in future")
+    matplotlib.pyplot.xlabel("days")
+    matplotlib.pyplot.ylabel("prices")
+    matplotlib.pyplot.legend(loc="best")
+    matplotlib.pyplot.savefig("plots/recurrent_for_multiple")
+    matplotlib.pyplot.close()
 
 
 def plot_recurrent_for_single(data, n_future, n_test, n_past):
@@ -54,12 +117,13 @@ def plot_recurrent_for_single(data, n_future, n_test, n_past):
         X_train, y_train = build_sequence(X_train, y_train, n_past)
         X_test, y_test = build_sequence(X_test, y_test, n_past)
 
-        y_pred = recurrent_lstm(X_train, X_test, y_train, y_test)
-        r2_score = sklearn.metrics.r2_score(y_test, y_pred)
+        for recurrent in get_recurrent_predictors():
+            y_pred = recurrent(X_train, X_test, y_train)
+            r2_score = sklearn.metrics.r2_score(y_test, y_pred)
 
-        print("recurrent lstm", "future day", day, "r2 score ", r2_score)
-        matplotlib.pyplot.figure(day)
-        matplotlib.pyplot.plot(y_pred, label="recurrent lstm")
+            print(recurrent_name(recurrent), "future day", day, "r2 score ", r2_score)
+            matplotlib.pyplot.figure(day)
+            matplotlib.pyplot.plot(y_pred, label=recurrent_name(recurrent))
         matplotlib.pyplot.plot(y_test, label="actual")
         matplotlib.pyplot.title("Predictions of recurrent for day in future " + str(day))
         matplotlib.pyplot.xlabel("days")
@@ -69,44 +133,11 @@ def plot_recurrent_for_single(data, n_future, n_test, n_past):
         matplotlib.pyplot.close()
 
 
-def plot_recurrent_for_multiple(data, n_test, n_past):
-    """
-    Plot the predictions for various recurrent techniques for multiple days in future.
-    :param data: quandl data
-    :param n_test: number of days for testing
-    :param n_past: number of days to remember in context
-    """
-    X, y = feature.build_sets(data, n_test)
-    X_train, X_test, y_train, y_test = feature.divide_sets(X, y, n_test + n_past + 1)
-    X_test, y_test = build_sequence(X_test, y_test, n_past)
-
-    y_pred_combined = []
-    for day in tqdm.tqdm(range(1, n_test + 1)):
-        X, y = feature.build_sets(data, day)
-        X_train, X_test, y_train, y_test = feature.divide_sets(X, y, n_test + n_past + 1)
-        X_train, y_train = build_sequence(X_train, y_train, n_past)
-        X_test, y_test = build_sequence(X_test, y_test, n_past)
-
-        y_pred = recurrent_lstm(X_train, X_test, y_train, y_test)
-        y_pred_combined.append(y_pred[day - 1])
-    r2_score = sklearn.metrics.r2_score(y_test, y_pred_combined)
-
-    print("recurrent lstm", "r2_score", r2_score)
-    matplotlib.pyplot.plot(y_pred_combined, label="recurrent lstm")
-    matplotlib.pyplot.plot(y_test, label="actual")
-    matplotlib.pyplot.title("Predictions of recurrent for days in future")
-    matplotlib.pyplot.xlabel("days")
-    matplotlib.pyplot.ylabel("prices")
-    matplotlib.pyplot.legend(loc="best")
-    matplotlib.pyplot.savefig("plots/recurrent_for_multiple")
-    matplotlib.pyplot.close()
-
-
 if __name__ == "__main__":
     numpy.random.seed(0)
     pathlib.Path("data").mkdir(parents=True, exist_ok=True)
     pathlib.Path("plots").mkdir(parents=True, exist_ok=True)
 
     data = feature.get_quandl_data("WIKI", "GOOGL")
-    plot_recurrent_for_single(data, 5, 100, 50)
-    plot_recurrent_for_multiple(data, 10, 50)
+    plot_recurrent_for_single(data, 5, 100, 500)
+    plot_recurrent_for_multiple(data, 10, 500)
